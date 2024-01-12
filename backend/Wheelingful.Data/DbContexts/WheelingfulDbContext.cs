1af394496a406e70ba022ef3b5088b477ce2c02d
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-01-12 13:58:28 +0200

    🛂 Integrate ASP.NET Core Identity

--- backend/Wheelingful.Data/DbContexts/WheelingfulDbContext.cs
+++ backend/Wheelingful.Data/DbContexts/WheelingfulDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Wheelingful.Data.DbContexts;
 
-internal class WheelingfulDbContext : DbContext
+internal class WheelingfulDbContext : IdentityDbContext<IdentityUser>
 {
     public WheelingfulDbContext(DbContextOptions<WheelingfulDbContext> options) 
         : base(options) { }
