c5e1844ce6e867ea15433e0783a1a168f77e589e
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-01-19 18:51:00 +0200

    🐛 Fix redux storage lacking persistance

--- frontend/src/app/components/app.jsx
+++ frontend/src/app/components/app.jsx
@@ -2,19 +2,22 @@ import AppWrapper from './app-wrapper';
 import { ThemeProvider } from '@mui/material';
 import { theme } from '../material-ui/theme';
 import { Provider } from 'react-redux';
-import { store } from '../store';
+import { persistor, store } from '../store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '../router/browser-router';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
   return (
     <AppWrapper>
       <ThemeProvider theme={theme}>
         <Provider store={store}>
-          <RouterProvider
-            router={router}
-            fallbackElement={<p>Initial Load...</p>}
-          />
+          <PersistGate loading={null} persistor={persistor}>
+            <RouterProvider
+              router={router}
+              fallbackElement={<p>Initial Load...</p>}
+            />
+          </PersistGate>
         </Provider>
       </ThemeProvider>
     </AppWrapper>
--- frontend/src/app/store/index.js
+++ frontend/src/app/store/index.js
@@ -1,15 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 import { authApi, authReducer } from '../../auth';
 
+const rootReducer = combineReducers({
+  authSlice: authReducer,
+  [authApi.reducerPath]: authApi.reducer,
+});
+
+const persistedReducer = persistReducer({
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: [authApi.reducerPath]
+}, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    authSlice: authReducer,
-    [authApi.reducerPath]: authApi.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(authApi.middleware);
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(authApi.middleware);
   },
 });
+
+export const persistor = persistStore(store);
 
 setupListeners(store.dispatch);
