419278dc60d083cea75d211d2b7a2760fdfdf3d1
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-01-28 18:19:47 +0200

    🗃️Add basic chapter entity + set global no-tracking policy

--- backend/Wheelingful.DAL/Entities/Abstract/BaseEntity.cs
+++ backend/Wheelingful.DAL/Entities/Abstract/BaseEntity.cs
@@ -1,6 +1,8 @@
 ﻿namespace Wheelingful.DAL.Entities.Abstract;
 
-public abstract class BaseEntity
+public abstract class BaseEntity : IBaseTimestamp
 {
     public int Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
---
+++ backend/Wheelingful.DAL/Migrations/20240128161314_Chapter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wheelingful.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Chapter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chapters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chapters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chapters_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chapters_BookId",
+                table: "Chapters",
+                column: "BookId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chapters");
+        }
+    }
+}
