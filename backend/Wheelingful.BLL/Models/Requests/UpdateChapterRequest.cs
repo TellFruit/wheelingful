94d86798b6b067e8fa33b53ebccccfdb702b2941
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-01-29 11:37:25 +0200

    👔 Add Chapter entity operations for Author

---
+++ backend/Wheelingful.BLL/Models/Requests/UpdateChapterRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Wheelingful.BLL.Models.Requests;
+
+public class UpdateChapterRequest
+{
+    public int Id { get; set; }
+    public int BookId { get; set; }
+    public required string Title { get; set; }
+}
