d28a69234030149cce447d6639bfa5fd55dd605d
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-01-29 11:37:25 +0200

    👔 Add Chapter entity operations for Author

---
+++ backend/Wheelingful.BLL/Contracts/Chapters/IChapterAuthorService.cs
@@ -0,0 +1,10 @@
+﻿using Wheelingful.BLL.Models.Requests;
+
+namespace Wheelingful.BLL.Contracts.Chapters;
+
+public interface IChapterAuthorService
+{
+    Task CreateChapter(CreateChapterRequest request);
+    Task UpdateChapter(UpdateChapterRequest request);
+    Task DeleteChapter(DeleteChapterRequest request);
+}
