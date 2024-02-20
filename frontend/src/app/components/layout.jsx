f176658f24ff3d884bcbb5e47b0edacb26d20406
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-02-20 17:56:24 +0200

    💄 Add sidebar menu for author actions

--- frontend/src/app/components/layout.jsx
+++ frontend/src/app/components/layout.jsx
@@ -1,7 +1,4 @@
-import {
-  Container,
-  Box,
-} from '@mui/material';
+import { CssBaseline, Container, Box, Toolbar } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import Header from './header.component';
 import Footer from './footer.component';
@@ -9,12 +6,14 @@ import Footer from './footer.component';
 export default function Layout() {
   return (
     <Box flexGrow={1}>
+      <CssBaseline />
       <Header />
       <Container
         component="main"
         maxWidth="lg"
-        sx={{ marginTop: 8, minHeight: '100vh' }}
+        sx={{ minHeight: '100vh', marginTop: 4 }}
       >
+        <Toolbar />
         <Outlet />
       </Container>
       <Footer />
--- frontend/src/book-publish/router/routes.js
+++ frontend/src/book-publish/router/routes.js
@@ -4,34 +4,37 @@ import BooksByCurrentUser from '../components/books-by-current-user.page';
 import ProtectedWrapper from '../../auth/components/protected.wrapper';
 import BookCreatePage from '../components/book-create.page';
 import BookUpdatePage from '../components/book-update.page';
+import PublishDrawer from '../components/publish-drawer.component';
 
 export default function PublishRouter() {
   return (
     <Routes>
-      <Route
-        path={PUBLISH_CONFIG.routes.booksByCurrentUser}
-        element={
-          <ProtectedWrapper>
-            <BooksByCurrentUser />
-          </ProtectedWrapper>
-        }
-      />
-      <Route
-        path={PUBLISH_CONFIG.routes.publishBook}
-        element={
-          <ProtectedWrapper>
-            <BookCreatePage />
-          </ProtectedWrapper>
-        }
-      />
-      <Route
-        path={PUBLISH_CONFIG.routes.updateBook}
-        element={
-          <ProtectedWrapper>
-            <BookUpdatePage />
-          </ProtectedWrapper>
-        }
-      />
+      <Route element={<PublishDrawer />} >
+        <Route
+          path={PUBLISH_CONFIG.routes.booksByCurrentUser}
+          element={
+            <ProtectedWrapper>
+              <BooksByCurrentUser />
+            </ProtectedWrapper>
+          }
+        />
+        <Route
+          path={PUBLISH_CONFIG.routes.publishBook}
+          element={
+            <ProtectedWrapper>
+              <BookCreatePage />
+            </ProtectedWrapper>
+          }
+        />
+        <Route
+          path={PUBLISH_CONFIG.routes.updateBook}
+          element={
+            <ProtectedWrapper>
+              <BookUpdatePage />
+            </ProtectedWrapper>
+          }
+        />
+      </Route>
     </Routes>
   );
 }
