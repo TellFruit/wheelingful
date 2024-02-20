b6457bd5a5367db9a9f478fa76e9dc9e0e6a34be
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-02-20 18:28:41 +0200

    🧑‍💻 Add enum string parsing for DTO

--- backend/Wheelingful.API/Models/Bindings/Bodies/UpdateBookBody.cs
+++ backend/Wheelingful.API/Models/Bindings/Bodies/UpdateBookBody.cs
@@ -1,4 +1,5 @@
-﻿using Wheelingful.DAL.Enums;
+﻿using System.Text.Json.Serialization;
+using Wheelingful.DAL.Enums;
 
 namespace Wheelingful.API.Models.Bindings.Bodies;
 
@@ -7,6 +8,11 @@ public class UpdateBookBody
     public required string Title { get; set; }
     public string? CoverBase64 { get; set; }
     public required string Description { get; set; }
+
+
+    [JsonConverter(typeof(JsonStringEnumConverter<BookCategoryEnum>))]
     public BookCategoryEnum Category { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter<BookStatusEnum>))]
     public BookStatusEnum Status { get; set; }
 }
--- backend/Wheelingful.BLL/Models/Responses/FetchBookResponse.cs
+++ backend/Wheelingful.BLL/Models/Responses/FetchBookResponse.cs
@@ -1,11 +1,18 @@
-﻿namespace Wheelingful.BLL.Models.Responses;
+﻿using System.Text.Json.Serialization;
+using Wheelingful.DAL.Enums;
+
+namespace Wheelingful.BLL.Models.Responses;
 
 public class FetchBookResponse
 {
     public int Id { get; set; }
     public required string Title { get; set; }
     public required string Description { get; set; }
-    public required string Category { get; set; }
-    public required string Status { get; set; }
     public required string CoverUrl { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter<BookCategoryEnum>))]
+    public BookCategoryEnum Category { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter<BookStatusEnum>))]
+    public BookStatusEnum Status { get; set; }
 }
