9203100b9a17e7b39de14c45563148eff62fed39
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-02-21 15:29:14 +0200

    💄 Add individual book page for reader

--- backend/Wheelingful.API/Models/Bindings/Bodies/UpdateBookBody.cs
+++ backend/Wheelingful.API/Models/Bindings/Bodies/UpdateBookBody.cs
@@ -9,7 +9,6 @@ public class UpdateBookBody
     public string? CoverBase64 { get; set; }
     public required string Description { get; set; }
 
-
     [JsonConverter(typeof(JsonStringEnumConverter<BookCategoryEnum>))]
     public BookCategoryEnum Category { get; set; }
 
