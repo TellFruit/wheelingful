1111a551487d5c2362190c1302474b65eee5380b
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-03-13 18:48:52 +0200

    👔 Handle CRUD operations of Reviews

---
+++ backend/Wheelingful.BLL/Models/Requests/DeleteReviewRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Wheelingful.BLL.Models.Requests;
+
+public class DeleteReviewRequest
+{
+    public int BookId { get; set; }
+    public required string UserId { get; set; }
+}
---
+++ backend/Wheelingful.BLL/Models/Requests/FetchReviewPaginationRequest.cs
@@ -0,0 +1,16 @@
+﻿using Wheelingful.BLL.Models.Requests.General;
+
+namespace Wheelingful.BLL.Models.Requests;
+
+public class FetchReviewPaginationRequest : FetchPaginationRequest
+{
+    public int? BookId { get; set; }
+    public bool? DoFetchByCurrentUser { get; set; }
+
+    public FetchReviewPaginationRequest(int? bookId, int? pageNumber, int? pageSize, bool? doFetchByCurrentUser)
+        : base(pageNumber, pageSize)
+    {
+        BookId = bookId;
+        DoFetchByCurrentUser = doFetchByCurrentUser;
+    }
+}
