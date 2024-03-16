dcd1d212ff0a4dcde0ea5f418c85dd750dde282c
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-03-16 16:46:32 +0200

    🗃️ Scuffold latest db scheme

---
+++ backend/Wheelingful.ML/Wheelingful.ML/Models/Db/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wheelingful.ML.Models.Db;
+
+public partial class Review
+{
+    public int BookId { get; set; }
+
+    public string UserId { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Text { get; set; } = null!;
+
+    public int Score { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual Book Book { get; set; } = null!;
+
+    public virtual Aspnetuser User { get; set; } = null!;
+}
