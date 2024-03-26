5d0fb3be648b1b9be9660e6cd2f3bfbe2e9e5c56
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-03-26 12:12:11 +0200

    🏗️ Set up integration tests

---
+++ backend/Wheelingful.IntegrationTests/Abstract/BaseTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.MySql;
+using Testcontainers.Redis;
+using Wheelingful.DAL.DbContexts;
+using Wheelingful.IntegrationTests.Constants;
+
+namespace Wheelingful.IntegrationTests.Abstract;
+
+public abstract class BaseTest : IAsyncLifetime
+{
+    private readonly MySqlContainer _mySqlContainer = new MySqlBuilder().Build();
+    private readonly RedisContainer _redisContainer = new RedisBuilder().Build();
+
+    protected WebApplicationFactory<Program> Factory { get; private set; } = null!;
+    protected HttpClient HttpClient { get; private set; } = null!;
+
+    public async Task InitializeAsync()
+    {
+        await _mySqlContainer.StartAsync();
+        await _redisContainer.StartAsync();
+
+        Environment.SetEnvironmentVariable("MYSQLCONNSTR_localdb", _mySqlContainer.GetConnectionString());
+
+        Factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(b =>
+            {
+                b.UseEnvironment(ApiConstants.BaseEnvironment);
+                b.UseSetting("ConnectionStrings:RedisConnection", _redisContainer.GetConnectionString());
+            });
+
+        HttpClient = Factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            BaseAddress = new Uri(ApiConstants.BaseUrl),
+        });
+
+        using var scope = Factory.Services.CreateScope();
+        await using var db = scope.ServiceProvider.GetRequiredService<WheelingfulDbContext>();
+        await db.Database.MigrateAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Factory.DisposeAsync();
+        await _mySqlContainer.DisposeAsync();
+        await _redisContainer.DisposeAsync();
+    }
+}
---
+++ backend/Wheelingful.IntegrationTests/Constants/ApiConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Wheelingful.IntegrationTests.Constants;
+
+public class ApiConstants
+{
+    public const string BaseEnvironment = "Development";
+    public const string BaseUrl = "https://localhost:7277";
+}
