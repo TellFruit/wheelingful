4a243d5a70bb0e75d7e9152e901613711cc1b9da
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-04-05 22:43:05 +0300

    🤡 Fake a cache service to save money on Redis Azure

---
+++ backend/Wheelingful.DAL/Services/FakeCacheService.cs
@@ -0,0 +1,21 @@
+﻿using Wheelingful.DAL.Contracts;
+
+namespace Wheelingful.DAL.Services;
+
+public class FakeCacheService : ICacheService
+{
+    public async Task<T> GetAndSet<T>(string key, Func<Task<T>> fetchData, TimeSpan expirationTime) where T : class
+    {
+        return await fetchData();
+    }
+
+    public Task RemoveByKey(string key)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task RemoveByPrefix(string prefix)
+    {
+        return Task.CompletedTask;
+    }
+}
