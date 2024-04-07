991695a94d3553a213d8dc5716996567ccfde986
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-04-07 12:03:27 +0300

    👔 Add machine learning instrument for matrix factorization

---
+++ backend/Wheelingful.ML/Wheelingful.ML/Models/ML/BookRating.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace Wheelingful.ML.Models.ML;
+
+public class BookRating
+{
+    [LoadColumn(1)]
+    public string UserId;
+    [LoadColumn(0)]
+    public float BookId;
+    [LoadColumn(2)]
+    public float ReviewScore;
+}
---
+++ backend/Wheelingful.ML/Wheelingful.ML/Models/ML/BookRatingPrediction.cs
@@ -0,0 +1,7 @@
+﻿namespace Wheelingful.ML.Models.ML;
+
+public class BookRatingPrediction
+{
+    public float Label { get; set; }
+    public float Score { get; set; }
+}
