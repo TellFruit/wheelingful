1a8bc19a273a6e6daa5024d1ca002e559522a69e
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-04-07 12:03:27 +0300

    👔 Add machine learning instrument for matrix factorization

--- backend/Wheelingful.ML/Wheelingful.ML/Constants/CsvConstants.cs
+++ backend/Wheelingful.ML/Wheelingful.ML/Constants/CsvConstants.cs
@@ -2,7 +2,9 @@
 
 public static class CsvConstants
 {
-    public const string FileToPopulateWithAmazonData = "Books_rating_amazon.csv";
-    public const string FileToPopulateWithFullParsedData = "Books_rating_full_parsed.csv";
-    public const string FileToPopulateWithLearningParsedData = "Books_rating_learning_parsed.csv";
+    public const string FileWithAmazonData = "Books_rating_amazon.csv";
+    public const string FileWithFullParsedData = "Books_rating_full_parsed.csv";
+    public const string FileWithFullFactorizationData = "Books_rating_full_factor_parsed.csv";
+    public const string FileWithTrainingFaztorizationData = "Books_rating_learning_factor_parsed.csv";
+    public const string FileWithTestingFaztorizationData = "Books_rating_testing_factor_parsed.csv";
 }
