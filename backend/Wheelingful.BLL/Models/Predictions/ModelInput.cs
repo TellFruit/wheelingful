deb89ed55fdf6b73506b4e011cda9291339fd829
TellFruit <ju1c7.d1g1t2@gmail.com>
2024-04-13 17:54:07 +0300

    ⚗️ Try training a model on API upon each request

---
+++ backend/Wheelingful.BLL/Models/Predictions/ModelInput.cs
@@ -0,0 +1,8 @@
+﻿namespace Wheelingful.BLL.Models.Predictions;
+
+public class ModelInput
+{
+    public required string UserId { get; set; }
+    public int BookId { get; set; }
+    public float ReviewScore { get; set; }
+}
---
+++ backend/Wheelingful.BLL/Models/Predictions/ModelOutput.cs
@@ -0,0 +1,7 @@
+﻿namespace Wheelingful.BLL.Models.Predictions;
+
+public class ModelOutput
+{
+    public float Label { get; set; }
+    public float Score { get; set; }
+}
